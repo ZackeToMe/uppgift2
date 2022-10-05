6e7035d3357fd019ae549177a43ae23ed1fc35de
Edvin Zackrisson <edvin@knack-knack.se>
2022-10-05 11:43:55 +0200

    Function to read and process file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ typedef struct currency {
     char *countryCode;
 } Currency;
 
-struct[] returnStructArray(char[] indata)
+struct returnStructArray(char[] indata)
 {
     Currency currencyStruct;
     int counter = 0;
@@ -16,26 +16,53 @@ struct[] returnStructArray(char[] indata)
     while (!feof(infile))
     {
         character = fgetc(infile);
-        if (counter == 3)
-        {
-            counter = 0;
-        }
         if (isalpha(character))
         {
             countryTemp[counter] = character;
+            switch (counter)
+            {
+                case 0:
+                    countryTemp[counter] = character;
+                    break;
+                case 1:
+                    countryTemp[counter] = character;
+                    break;
+                case 2:
+                    countryTemp[counter] = character;
+                    break;
+                case 3:
+                    countryTemp[counter] = '\0';
+                    counter = 0;
+
+                    break;
+            }
         }
-        else
+        if else (isdigit(character))
         {
             valueTemp = character;
             switch (counter)
             {
                 case 0:
-                    valueTemp = character*100;
+                    valueTemp += character*100;
+                    break;
+                case 1:
+                    valueTemp += character*10;
+                    break;
+                case 2:
+                    valueTemp += character*1;
+                    break;
+                case 3:
+                    counter = 0;
+                    break;
             }
         }
+        else
+        {
+
+        }
         currencyStruct.countryCode = countryTemp;
+        currencyStruct.value = valueTemp;
     }
-    fread()
 }
 
 int main() {
